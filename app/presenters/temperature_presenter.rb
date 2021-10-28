0b25b78e1ca987b10c5e8c63486e203052abe629
Ash Barton <ashbarton5@gmail.com>
2021-10-28 10:00:30 +0100

    postcode check and reformat added, other minor additions

--- app/presenters/temperature_presenter.rb
+++ app/presenters/temperature_presenter.rb
@@ -1,7 +1,9 @@
 class TemperaturePresenter
   include HTTParty
 
-  def initialize()
+  def initialize(postcode)
+    # @postcode = postcode_reformat(:postcode)
+    @response = ("https://api.weatherapi.com/v1/current.json?key=55b7fdf17805493199a143223212409" + "&#{@postcode}")
   end
 
   def hot_definition
@@ -17,13 +19,30 @@ class TemperaturePresenter
   end
 
   def postcode_temperature
-    response = HTTParty.get('https://api.weatherapi.com/v1/current.json?key=55b7fdf17805493199a143223212409&q=ip130sr&days=0')
-    temperature = response['current']['temp_c']
+    @response['current']['temp_c'] ? uk_postcode_valid(@postcode) : 'Please enter a valid UK postcode'
+    # base_uri = 'https://api.weatherapi.com/v1/current.json?key=55b7fdf17805493199a143223212409')
+    # response = base_uri + "&#{@postcode}"
+    # response = HTTParty.get('https://api.weatherapi.com/v1/current.json?key=55b7fdf17805493199a143223212409&q=ip130sr')
+    temperature = @response['current']['temp_c']
     temperature
   end
 
-  def postcode_check
+  def uk_postcode_valid
+    case @response
+    when @response['error'].present?
+      false
+    when @reponse['location']['country'] != 'UK'
+      false
+    else
+      true
+    end
 
+    # true ? @response['error'].blank? : false
+  end
+
+  def postcode_reformat(postcode)
+    reformatted_postcode = postcode.gsub(/\s+/, "").downcase
+    reformatted_postcode
   end
 
 end
