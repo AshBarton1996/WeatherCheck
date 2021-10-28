426687dd96d85e11cf7901158b970ef910e58a19
Ash Barton <ashbarton5@gmail.com>
2021-10-28 13:11:53 +0100

    added search routes and updated controllers

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,4 +1,10 @@
 class WeatherController < ApplicationController
   def index
+    @presenter = TemperaturePresenter.new(postcode: nil)
+  end
+
+  def search
+    @presenter = TemperaturePresenter.new(postcode: params[:query])
+    render action: :index
   end
 end
