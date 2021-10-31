9cd2e6da513b606240096e81804dcba52262531e
Ash Barton <ashbarton5@gmail.com>
2021-10-31 20:35:59 +0000

    Presenter specs updated and initialisation error fixed

--- spec/presenters/temperature_presenter_spec.rb
+++ spec/presenters/temperature_presenter_spec.rb
@@ -1,13 +1,43 @@
 # frozen_string_literal: true
+require 'rails_helper'
 
-describe 'temperature_presenter' do
+describe TemperaturePresenter do
   before do
-    @valid_postcode_presenter = TemperaturePresenter.new(postcode: 'CT6 6YY')
-    @invalid_postcode_presenter = TemperaturePresenter.new(postcode: 'CT6 6YY')
-    @no_postcode_presenter = TemperaturePresenter.new(postcode: 'CT6 6YY')
+    @valid_postcode_presenter =  TemperaturePresenter.new(postcode: 'CT6 6YY')
+    @invalid_postcode_presenter = TemperaturePresenter.new(postcode: 'CT6 6YYKR')
+    @no_postcode_presenter = TemperaturePresenter.new(postcode: nil)
+
+    # API output data used to check method testing
+    @response = HTTParty.get('https://api.weatherapi.com/v1/current.json?key=55b7fdf17805493199a143223212409&q=ct66yy')
+    @hot_warm_cold = @valid_postcode_presenter.hot_warm_cold?
+    @postcode_temperature = @response['current']['temp_c']
   end
 
-  describe 'uk_postcode_valid?' do
+  describe '.hot_definition' do
+    it 'returns the definition of hot' do
+      expect(@valid_postcode_presenter.hot_definition).to eq("Anything above 20 degrees. Getting to the hot as balls range.")
+    end
+  end
+
+  describe '.warm_definition' do
+    it 'returns the definition of warm' do
+      expect(@valid_postcode_presenter.warm_definition).to eq("Anything between 10 and 20 degrees. Maybe a temperature english people won't actually moan about.")
+    end
+  end
+
+  describe '.cold_definition' do
+    it 'returns the definition of cold' do
+      expect(@valid_postcode_presenter.cold_definition).to eq("Anything below 10 degrees. Much lower and Pengu would be comfortable.")
+    end
+  end
+
+  describe '.postcode_temperate' do
+    it 'returns the current temperature of a given postcode' do
+      expect(@valid_postcode_presenter.postcode_temperature).to eq(@postcode_temperature)
+    end
+  end
+
+  describe '.uk_postcode_valid?' do
     context 'when valid uk postcode is given' do
       it 'returns true' do
         expect(@valid_postcode_presenter.uk_postcode_valid?).to eq(true)
@@ -31,7 +61,7 @@ describe 'temperature_presenter' do
     end
   end
 
-  describe 'postcode_reformat' do
+  describe '.postcode_reformat' do
     context 'when wrong formatted postcode is given' do
       it 'returns correct formatted postcode (all lower case and no spaces)' do
         expect(@no_postcode_presenter.postcode_reformat('CT6 6YY')).to eq('ct66yy')
@@ -39,14 +69,8 @@ describe 'temperature_presenter' do
     end
   end
 
-  describe 'hot_or_not_statement' do
+  describe '.hot_or_not_statement' do
     context 'when a correct UK address has been searched' do
-      before do
-        @response = HTTParty.get('https://api.weatherapi.com/v1/current.json?key=55b7fdf17805493199a143223212409&q=ct66yy')
-        @hot_warm_cold = @valid_postcode_presenter.hot_warm_cold?
-        @postcode_temperature = @valid_postcode_presenter.postcode_temperature
-      end
-
       it 'returns the correct weather statement' do
         expect(@valid_postcode_presenter.hot_or_not_statement).to eq("The temperature is #{@postcode_temperature} degrees and therefor it is #{@hot_warm_cold} in your local area.")
       end
